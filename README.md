b1ed1b8eb7504a65bb521c1c5136a94e9eec98b6
Jayaprakash Jayakumar <jayaprakash2050@gmail.com>
2016-01-18 11:25:38 -0700

    added comments

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # EventbriteChromeExtension
 Chrome extension for Eventbrite.  Fetch popular events from Eventbrite based on the users location.
 Used Google Maps API, Eventbrite API.
-Documentation and user guide to be typed.
+Also fetches the events that happens only on next weekend.
+
+To install:
+Unzip the extension folder.
+Open Extenions tab in Chrome.
+Enable developer mode.
+Drag and drop the folder on to the Chrome.
+The extension will be loaded and available next to the omnibar in Chrome.
+
+Please type the location in the textbox and select the location from autocomplete suggestions.
+The popular events happening in that location are displayed below. 
+The default distance used is 10 miles, it can be selected from the dropdown present.
+If next weekend check box is selected then the events that happen only on next weekend will be displayed.
--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 //Variable declarations
-var placeSearch, autocomplete;
+var placeSearch, autoComplete;
 var evenBriteToken = "326SDDB4XLUPNNP3NJR4";
 var daysOfWeek = new Array(7);
-daysOfWeek = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 var monthNames = new Array(12);
-monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+monthNames = 
+["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 //HTML template to fill the Event details on the page
 var template = 
-"<li class = 'listItem'>\
+"<li class = 'list-item'>\
 		<a class = 'hyperlink' href='##eventurl##' target = '_blank'>\
-			<div class = 'itemDiv'>\
-				<div class = 'imgDiv'>\
-					<img src = '##imageurl##' class = 'imageclass'/>\
+			<div class = 'item-div'>\
+				<div class = 'img-div'>\
+					<img src = '##imageurl##' class = 'imageclass' />\
 				</div>\
-				<div class = 'titleDiv'>\
+				<div class = 'title-div'>\
 					<span class = 'titlespan'>\
 						##eventtitle##\
 					</span>\
-					<br/>\
-					<br/>\
+					<br />\
+					<br />\
 					<span class = 'date'>\
 						##date##\
 					</span>\
@@ -36,7 +37,7 @@ var opts = {
 , corners: 1 // Corner roundness (0..1)
 , color: '#000' // #rgb or #rrggbb or array of colors
 , opacity: 0.1 // Opacity of the lines
-, rotate: 8 // The rotation offset
+, rotate: 8 // The rotation offset 	
 , direction: 1 // 1: clockwise, -1: counterclockwise
 , speed: 1 // Rounds per second
 , trail: 50 // Afterglow percentage
@@ -64,136 +65,147 @@ function geolocate() {
 				center: geolocation,
 				radius: position.coords.accuracy
 			});
-			autocomplete.setBounds(circle.getBounds());
+			autoComplete.setBounds(circle.getBounds());
 		});
 
 	}
 }
 //This is the callback method for Google maps API
 function initAutoComplete() {
-	autocomplete = new google.maps.places.Autocomplete(
+	autoComplete = new google.maps.places.Autocomplete(
 		(document.getElementById('location')),{types: ['geocode']});
-	autocomplete.addListener('place_changed', populateEvents);
+	autoComplete.addListener('place_changed', populateEvents);
 }
 
 //Event handler for Next weekend check box.
 //This fetches all events that will happen on next weekend on selection of that checkbox,
-$(document).on("change", "input[id='chkNextWeekend']", function () {
-	var place = autocomplete.getPlace();
+$(document).on("change", "input[id='chknext-weekend']", function () {
+	var place = autoComplete.getPlace();
 	if( place != undefined && $("#location").val()) {
 		populateEvents();
 	}
 	else
 	{
-		alert("Enter Location and try");
+		//do nothing
 	}
 });
 
 //Event handler for distance dropdown.
 //This fetches the events that will happen within the given distance from given location
 $(document).on("change", "select[id='distance']", function () {
-	var place = autocomplete.getPlace();
+	var place = autoComplete.getPlace();
 	if( place != undefined && $("#location").val()) {
 		populateEvents();
 	}
 	else
 	{
-		alert("Enter Location and try");
+		//do nothing
 	}
 });
 
 //Function to add geolocate() method as event listener for location textbox, for onFocus event
-$(document).ready(function(){
+$(document).ready(function() {
 	document.getElementById("location").addEventListener("onFocus", geolocate);
 });
 
 //This function calls the Eventbrite API to fetch the events based on the user selection. 
 function populateEvents() {
-	var place = autocomplete.getPlace();
+	var place = autoComplete.getPlace();
 	var latitude = place.geometry.location.lat();
 	var longitude = place.geometry.location.lng();
 	var spinner = '';
 	//default distance to find events is 10 miles
 	var distanceSelected = 10;	
 	distanceSelected = $("#distance").val();
-	var url = 'https://www.eventbriteapi.com/v3/events/search/?token='+evenBriteToken+'&location.latitude='+latitude+'&location.longitude='+longitude+'&location.within='+distanceSelected+'mi&popular=true';
+	var url = 'https://www.eventbriteapi.com/v3/events/search/?token='+ evenBriteToken + 
+	'&location.latitude=' + latitude + '&location.longitude=' + longitude + 
+	'&location.within=' + distanceSelected + 'mi&popular=true';
+	var nextSat, nextSun;
 	/*If next weekend filter is selected then we calculate the next weekend dates and add it as 
 	  condition for the Eventbrite API call.
 	*/
-	if ( $("#chkNextWeekend").prop("checked") ) {
-		var nextSat = calculateNextWeekend();
-		var nextSun = new Date();
+	if ( $("#chknext-weekend").prop("checked") ) {
+		nextSat = calculateNextWeekend();
+		nextSun = new Date();
 		nextSun.setDate(nextSat.getDate() + 1);
 		nextSun.setHours(23,59,59, 999);
 		nextSat = nextSat.toJSON();
 		nextSat = nextSat.substring(0,nextSat.lastIndexOf('.'))+'Z';
 		nextSun = nextSun.toJSON();
 		nextSun = nextSun.substring(0,nextSun.lastIndexOf('.'))+'Z';
-		url = url + '&start_date.range_start='+encodeURIComponent(nextSat)+'&start_date.range_end='+encodeURIComponent(nextSun);
+		url = url + '&start_date.range_start=' + encodeURIComponent(nextSat) + 
+		'&start_date.range_end=' + encodeURIComponent(nextSun);
 	}
 	//Ajax call for Eventbrite API to fetch the events
 	$.ajax( { 
 		url: url,
 		beforeSend: function(xhr) {
-			var target = document.getElementById("contentDiv");
+			var target = document.getElementById("content-div");
 			spinner = new Spinner(opts);
 			spinner.spin(target);
 		},
 		error: function(xhr) {
 			spinner.stop();	
-			$("#contentDiv").html("<p>Some exception occured.</p>");
+			$("#content-div").html("<h2>Some exception occured.</h2>");
 		}
 	})
 		.done( function( data ) {
 			//After recieving the data, read the data and populate the events template and 
 			//display it in the pop up.
 			spinner.stop();	
-			var divContent = "<ul class='eventList'>";		
-			$.each(data.events, function( key, event ){
-				var eventName = event.name.html;
-				//Only 100 characters of event name will be displayes in the page.
-				if (eventName.length > 100) {
-					eventName = eventName.substr(0,100);
-				}
-				var startDate = new Date(event.start.utc);
-				var timeZone = event.start.timezone;
-				var logoURL = '';
-				if( event.logo ){
-					logoURL = event.logo.url;
-				}
-				var eventURL = event.url;
-				startDate = formatDate(startDate);
-				var listItemTemplate = template;
-				listItemTemplate = listItemTemplate.replace('##eventurl##', eventURL);
-				listItemTemplate = listItemTemplate = listItemTemplate.replace('##imageurl##', logoURL);
-				listItemTemplate = listItemTemplate.replace('##eventtitle##',eventName);
-				listItemTemplate = listItemTemplate.replace('##date##',startDate);
-				divContent = divContent + listItemTemplate;
-			} );
-				divContent = divContent + "</ul>";
-				$("#contentDiv").html(divContent);
+			var divContent = "<ul class='event-list'>";
+			var eventName, startDate, timeZone, logoURL, eventURL, listItemTemplate;
+			if (data.events.length < 1) {
+				$("#content-div").html("<h2>No events found.</h2>");
+			}
+			else {
+				$.each(data.events, function( key, event ){
+					eventName = event.name.html;
+					//Only 100 characters of event name will be displayes in the page.
+					if (eventName.length > 100) {
+						eventName = eventName.substr(0,100);
+					}
+					startDate = new Date(event.start.utc);
+					timeZone = event.start.timezone;
+					logoURL = '';
+					if( event.logo ){
+						logoURL = event.logo.url;
+					}
+					eventURL = event.url;
+					startDate = formatDate(startDate);
+					listItemTemplate = template;
+					listItemTemplate = listItemTemplate.replace('##eventurl##', eventURL);
+					listItemTemplate = listItemTemplate.replace('##imageurl##', logoURL);
+					listItemTemplate = listItemTemplate.replace('##eventtitle##',eventName);
+					listItemTemplate = listItemTemplate.replace('##date##',startDate);
+					divContent = divContent + listItemTemplate;
+				} );
+					divContent = divContent + "</ul>";
+					$("#content-div").html(divContent);
+			}
 		})
 		.fail( function() {
 			spinner.stop();	
-			$("#contentDiv").html("<h2>Some exception occured.</h2>");
+			$("#content-div").html("<h2>Some exception occured.</h2>");
 		});
 }
 //To format the date for displaying it in the page.
-function formatDate( date ){
-	var d = date.getDate();
-	var day = date.getDay();
-	var timeHours = date.getHours();
-	var timeMinutes = date.getMinutes();
+function formatDate( date ) {
+	var d, day, timeHours, timeMinutes, month, am, result;
+	d = date.getDate();
+	day = date.getDay();
+	timeHours = date.getHours();
+	timeMinutes = date.getMinutes();
 	timeMinutes = timeMinutes == '0' ? '00' : timeMinutes;
-	var month = monthNames[date.getMonth()];
-	var am = timeHours >= 12 ? 'PM' : 'AM';
+	month = monthNames[date.getMonth()];
+	am = timeHours >= 12 ? 'PM' : 'AM';
 	timeHours = timeHours > 12 ? timeHours-12 : timeHours;
 	day = daysOfWeek[day];
-	var result = day + ', ' + month + ' ' + d + ' ' + timeHours + ':' + timeMinutes + ' ' + am;
+	result = day + ', ' + month + ' ' + d + ' ' + timeHours + ':' + timeMinutes + ' ' + am;
 	return result;  
 }
 //Function to calculate the next weekend based on the current date.
-function calculateNextWeekend(){
+function calculateNextWeekend() {
 	var date = new Date();
 	date.setDate((date.getDate() + (7 + 6 - date.getDay()) % 7) + 7);
 	date.setHours(00,00,00, 000);
